7ca3a623b1b117c0c9ed49b9bd57882116a5f2e8
vvvv-dottore <natan.sinigaglia@gmail.com>
2019-07-22 12:43:04 +0200

    Copied library from temp repo

---
+++ src/VL.Particles/Enums.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vl.Particles.Core
+{
+    public enum TargetProperty
+    {
+        To_Force, To_Acceleration, To_Velocity, To_Position
+    }
+
+    public enum OperationType
+    {
+        Add, Subtract, Multiply, Divide, Min, Max
+    }
+
+}
